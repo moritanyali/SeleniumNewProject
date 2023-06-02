04807028c750696090b63790f1bd5a3796ee12a3
Oguzhan Aydin <moritanyali26@yahoo.com>
2023-06-03 00:12:33 +0300

    27/05/23

--- src/test/java/techproed/day06_relativeLocators/C01_relativeLocators.java
+++ src/test/java/techproed/day06_relativeLocators/C01_relativeLocators.java
@@ -1,2 +1,62 @@
-package techproed.day06_relativeLocators;public class C01_relativeLocators {
+package techproed.day06_relativeLocators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
+
+import static org.openqa.selenium.support.locators.RelativeLocator.with;
+
+public class C01_relativeLocators {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        //amazona gidelim
+        driver.get("https://amazon.com");
+
+        //city bike aratalım
+        WebElement aramaKutusu = driver.findElement(By.xpath("//*[@type='text']"));
+        aramaKutusu.sendKeys("city bike", Keys.ENTER);
+
+        //relative locator kullanarak hybrid bikes altındaki elemente tıklayalım
+        //Webelementlerin yazısını konsola yazdıralım.
+        WebElement hybridBike = driver.findElement(By.xpath("//*[text()='Hybrid Bikes']"));
+        System.out.println(hybridBike.getText());
+        WebElement electricBike = driver.findElement(with(By.tagName("a")).below(hybridBike));
+        System.out.println(electricBike.getText());
+        WebElement completeBike = driver.findElement(with(By.tagName("a")).below(electricBike));
+        System.out.println(completeBike.getText());
+
+        //Bikes altındaki tüm linkleri(webelement) konsola yazdıralım
+        List<WebElement> linlkerListesi = driver.findElements(By.xpath("(//*[@class='a-list-item'])[position()>=10 and position()<=16]"));
+        //index no 10 16 arasini alir loop gibi.
+         /*
+            XPATH kullaniminda bir sayfadaki webelementlere mudehala etmek istedigimizda xpath ile locate aldigimizda birden fazla sonuc
+        verebilir. [position()>=9 and position()<=15] bu kullanimla aralik belirterek istedigimiz webelementleri secip mudehale edebiliriz.
+         */
+        //lambda ile yazdırdık
+        linlkerListesi.forEach(t-> System.out.println(t.getText()));
+
+
+        //Bu linklerin hepsini tıklayalım ve sayfa başlıklarını alalım
+        for (int i = 0; i < linlkerListesi.size(); i++) {
+            linlkerListesi = driver.findElements(By.xpath("(//*[@class='a-list-item'])[position()>=10 and position()<=16]"));
+            System.out.println(linlkerListesi.get(i).getText());
+            linlkerListesi.get(i).click();
+            Thread.sleep(3000);
+            driver.navigate().back();
+            Thread.sleep(3000);
+        }
+
+
+
+        completeBike.click();
+
+    }
 }
